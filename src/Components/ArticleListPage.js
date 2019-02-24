71fe7d1d34123299c5e33264283e8ad1a820e462
lourod1987 <louart87@gmail.com>
2019-02-23 22:47:55 -0600

    initializing state for an empty array. setting state in componentDidMount after data recieved from fetch. Passing in state to Cards component as a prop

--- src/Components/ArticleListPage.js
+++ src/Components/ArticleListPage.js
@@ -4,17 +4,28 @@ import Cards from './Cards';
 import Footer from './Footer';
 
 export default class ArticleListPage extends Component {
+    state = {
+        articles: []
+    }
     componentDidMount(){
         const apiKey = '2d2509aeb33d472da6f8f1cc4c4aa211';
         fetch(`https://newsapi.org/v2/top-headlines?sources=engadget&apiKey=${apiKey}`)
         .then(res => res.json())
-        .then(article => console.log(article))
+        .then(data => {
+            // console.log(data.articles)
+            console.log(this.state.articles)
+            this.setState({
+                articles: data.articles
+            })
+            console.log(this.state.articles)
+        })
     }
     render() {
+        const { articles } = this.state;
         return (
             <div>
                 <Navbar />
-                <Cards />
+                <Cards articles={articles}/>
                 <Footer />
             </div>
 
