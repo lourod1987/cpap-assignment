d133d5c3fb17fdf4b43f81dfd48ce55d0c58b85d
lourod1987 <louart87@gmail.com>
2019-02-23 21:23:24 -0600

    adding link to home page to the site title and User Info page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
     render() {
         return (
             <header>
                 <img src="" alt=""/>
-                <h1>Aggregated News</h1>
+                <Link to="/"><h1>Aggregated News</h1></Link>
                 <input type="text"  placeholder="Search"/>
                 <nav>
+                        <Link to="/userinfo">User Info</Link>
                     <ul>
                         <li>Sign Up</li>
                         <li>Get the App</li>
