5f720bbf3727f25ae3fc6c80e13d796579e24fec
lourod1987 <louart87@gmail.com>
2019-02-24 14:12:46 -0600

    changing spaces intendentation from 2 to 4

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
-    render() {
-        return (
-                <header>
-                    <nav>
-                        <img src="" alt=""/>
-                        <Link to="/"><h1>Aggregated News</h1></Link>
-                        <input type="text"  placeholder="Search"/>
-                                <Link to="/userinfo" className="link">User Info</Link>
-                        {/* <ul>
-                            <li>Sign Up</li>
-                            <li>Get the App</li>
-                            <li>Log in</li>
-                        </ul> */}
-                    </nav>
-                </header>
-        );
-    }
+  render() {
+    return (
+      <header>
+        <nav>
+          <img src="" alt=""/>
+          <Link to="/"><h1>Aggregated News</h1></Link>
+          <input type="text"  placeholder="Search"/>
+          <Link to="/userinfo" className="link">User Info</Link>
+          {/* <ul>
+              <li>Sign Up</li>
+              <li>Get the App</li>
+              <li>Log in</li>
+          </ul> */}
+        </nav>
+      </header>
+    );
+  }
 }
--- src/Components/UserInfoPage.js
+++ src/Components/UserInfoPage.js
@@ -3,20 +3,20 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 
 export default class UserInfoPage extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <main className="maininfo">
-                    <h2>User Info</h2>
-                    <p>User has visited the following articles:</p>
-                    <p>Temp display of articles tracked</p>
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <main className="maininfo">
+          <h2>User Info</h2>
+          <p>User has visited the following articles:</p>
+          <p>Temp display of articles tracked</p>
 
-                    <p>User has viewed the following full articles at engadget site:</p>
-                    <p>Temp display of tracked clicks on external article link</p>
-                </main>
-                <Footer />
-            </div>
-        );
-    }
+          <p>User has viewed the following full articles at engadget site:</p>
+          <p>Temp display of tracked clicks on external article link</p>
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
