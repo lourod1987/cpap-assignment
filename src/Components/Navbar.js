bafee1890672e8f2bf9d1513f22e88d2f83154d8
lourod1987 <louart87@gmail.com>
2019-02-24 00:57:13 -0600

    commenting out some links for now and restructuring elements

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,19 +4,19 @@ import { Link } from 'react-router-dom';
 export default class Navbar extends Component {
     render() {
         return (
-            <header>
-                <img src="" alt=""/>
-                <Link to="/"><h1>Aggregated News</h1></Link>
-                <input type="text"  placeholder="Search"/>
-                <nav>
-                        <Link to="/userinfo">User Info</Link>
-                    <ul>
-                        <li>Sign Up</li>
-                        <li>Get the App</li>
-                        <li>Log in</li>
-                    </ul>
-                </nav>
-            </header>
+                <header>
+                    <nav>
+                        <img src="" alt=""/>
+                        <Link to="/"><h1>Aggregated News</h1></Link>
+                        <input type="text"  placeholder="Search"/>
+                                <Link to="/userinfo" className="link">User Info</Link>
+                        {/* <ul>
+                            <li>Sign Up</li>
+                            <li>Get the App</li>
+                            <li>Log in</li>
+                        </ul> */}
+                    </nav>
+                </header>
         );
     }
 }
