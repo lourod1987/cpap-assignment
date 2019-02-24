fef8e3c92f3f665db65d0b91bd35571856bcecdf
lourod1987 <louart87@gmail.com>
2019-02-24 13:19:37 -0600

    removing commented out code. importing ArticleContainer which is now source data container for pages

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
 import React, { Component } from 'react';
-import ArticleListPage from './Components/ArticleListPage';
-import UserInfoPage from './Components/UserInfoPage';
-import { Route } from 'react-router-dom';
+import ArticleContainer from './Components/ArticleContainer';
 // import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Route exact path="/" render={ () => (
-          <ArticleListPage />
-        )}/>
-        <Route exact path="/userinfo" render={ () => (
-          <UserInfoPage />
-        )}/>
-      </div>
-       /* <div className="App">
-         <header className="App-header">
-           <img src={logo} className="App-logo" alt="logo" />
-         <p>
-             Edit <code>src/App.js</code> and save to reload.
-           </p>
-           <a
-             className="App-link"
-             href="https://reactjs.org"
-             target="_blank"
-             rel="noopener noreferrer"
-           >
-             Learn React
-           </a>
-         </header>
-       </div> */
+      <ArticleContainer />
     );
   }
 }
