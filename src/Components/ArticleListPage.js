e935c072699e0130b3b6614d5ab50f332898c450
lourod1987 <louart87@gmail.com>
2019-02-24 13:23:18 -0600

    moved state and componentdidmount up into a container file. deconstructed new props and passed them into cards component

--- src/Components/ArticleListPage.js
+++ src/Components/ArticleListPage.js
@@ -4,32 +4,16 @@ import Cards from './Cards';
 import Footer from './Footer';
 
 export default class ArticleListPage extends Component {
-    state = {
-        articles: []
-    }
-    componentDidMount(){
-        const apiKey = '2d2509aeb33d472da6f8f1cc4c4aa211';
-        // const spareUrl = `https://newsapi.org/v2/everything?q=tech&apiKey=${apiKey}`
-        fetch(`https://newsapi.org/v2/top-headlines?sources=engadget&apiKey=${apiKey}`)
-        .then(res => res.json())
-        .then(data => {
-            // console.log(data.articles)
-            console.log(this.state.articles)
-            this.setState({
-                articles: data.articles
-            })
-            console.log(this.state.articles)
-        })
-    }
-    render() {
-        const { articles } = this.state;
-        return (
-            <div>
-                <Navbar />
-                <Cards articles={articles}/>
-                <Footer />
-            </div>
-
-        );
-    }
+    
+  render() {   
+    const { articles, generateUrl, url, goToArticle } = this.props;
+    return (
+        <div>
+            <Navbar />
+            <Cards articles={articles} goToArticle={goToArticle} url={url} />
+            {/* <Cards articles={articles} generateUrl={generateUrl} url={url} /> */}
+            <Footer />
+        </div>
+    );
+  }
 }
