7666e06aa528db7df0dce2d26220f42db2a82007
lourod1987 <louart87@gmail.com>
2019-02-23 21:26:41 -0600

    no actual changes occurred for this commit

---
+++ src/Components/UserInfoPage.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+export default class UserInfoPage extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <main>
+                    <h2>User Info</h2>
+                    <p>User has visited the following articles:</p>
+                    <p>Temp display of articles tracked</p>
+
+                    <p>User has viewed the following full articles at engadget site:</p>
+                    <p>Temp display of tracked clicks on external article link</p>
+                </main>
+                <Footer />
+            </div>
+        );
+    }
+}
