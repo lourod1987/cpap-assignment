b2254a23b9e03310862c8b02e8444ece85420469
lourod1987 <louart87@gmail.com>
2019-02-24 14:10:00 -0600

    changing spaces intendentation from 2 to 4

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -2,32 +2,31 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Cards extends Component {
-    render() {
-        const { articles, generateUrl, url, goToArticle } = this.props;
-        return (
-            <div>
-                {/* <div>
-                    {articles.map( articles => (
-                        <img src={article} alt=""/>
-                        <p>Company Name Filler</p>
-                    ))}
-                </div> */}
-                <ul className="cards">
-                    {articles.map( (article, index) => (
-                        <li key={`${article.publishedAt}+`}>
-                            <Link to="/article">
-                                <img src={article.urlToImage} onClick={() => goToArticle(index)} alt=""/>
-                                {/* <img src={article.urlToImage} onClick={() => this.props.generateUrl(article.source.name, article.author, article.publishedAt)} alt=""/> */}
-                            </Link>
-                            {/* <p>Hashtag Temp</p> */}
-                            <h2>{article.title}</h2>
-                            <h3>{`${article.source.name} - ${article.author}`}</h3>
-                            <p>{article.content}</p>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-
-        );
-    }
+  render() {
+    const { articles, generateUrl, url, goToArticle } = this.props;
+    return (
+      <div>
+        {/* <div>
+            {articles.map( articles => (
+                <img src={article} alt=""/>
+                <p>Company Name Filler</p>
+            ))}
+        </div> */}
+        <ul className="cards">
+          {articles.map( (article, index) => (
+            <li key={`${article.publishedAt}+`}>
+              <Link to="/article">
+                <img src={article.urlToImage} onClick={() => goToArticle(index)} alt=""/>
+                {/* <img src={article.urlToImage} onClick={() => this.props.generateUrl(article.source.name, article.author, article.publishedAt)} alt=""/> */}
+              </Link>
+              {/* <p>Hashtag Temp</p> */}
+              <h2>{article.title}</h2>
+              <h3>{`${article.source.name} - ${article.author}`}</h3>
+              <p>{article.content}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
