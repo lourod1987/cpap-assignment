10ac85b6b3e0b15d64aa9d5b23c074015e7fad5d
lourod1987 <louart87@gmail.com>
2019-02-23 21:25:19 -0600

    setting up and testing api call for data that needs to be retrieved to complete rendering the ui

--- src/Components/ArticleListPage.js
+++ src/Components/ArticleListPage.js
@@ -4,6 +4,12 @@ import Cards from './Cards';
 import Footer from './Footer';
 
 export default class ArticleListPage extends Component {
+    componentDidMount(){
+        const apiKey = '2d2509aeb33d472da6f8f1cc4c4aa211';
+        fetch(`https://newsapi.org/v2/top-headlines?sources=engadget&apiKey=${apiKey}`)
+        .then(res => res.json())
+        .then(article => console.log(article))
+    }
     render() {
         return (
             <div>
