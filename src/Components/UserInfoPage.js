c9570531143421730a4f1480f93323cf2e0f99b2
lourod1987 <louart87@gmail.com>
2019-02-25 04:28:30 -0600

    passing in props. Displaying two lists, what renders is dependent on whether there is any data in the props currently

--- src/Components/UserInfoPage.js
+++ src/Components/UserInfoPage.js
@@ -4,16 +4,41 @@ import Footer from './Footer';
 
 export default class UserInfoPage extends Component {
   render() {
+    const { visited, viewedFull } = this.props;
     return (
       <div>
         <Navbar />
         <main className="maininfo">
           <h2>User Info</h2>
-          <p>User has visited the following articles:</p>
-          <p>Temp display of articles tracked</p>
-
-          <p>User has viewed the following full articles at engadget site:</p>
-          <p>Temp display of tracked clicks on external article link</p>
+          {visited.length === 0 ?
+          (<div className="no-results">
+            <p>User has visited the following articles:</p>
+            <p>No results yet</p>
+          </div>)
+          :
+          (<ol>
+            <p>User has visited the following articles:</p>
+            {visited.map( (v) => (
+              <li key={v.viewed}>
+                <p>{`${v.viewed}`}</p>
+                
+              </li>
+            ))}
+          </ol>)}
+          {viewedFull.length === 0 ?
+          (<div className="no-results">
+            <p>User has viewed the following full articles at Engagdet:</p>
+            <p>No results yet</p>
+          </div>)
+          :
+          (<ol>
+              <p>User has viewed the following full articles at Engagdet:</p>
+            {viewedFull.map( (vF) => (
+              <li key={vF.fullArticle}>
+                <p>{`${vF.fullArticle}`}</p>
+              </li>
+            ))}
+          </ol>)}
         </main>
         <Footer />
       </div>
