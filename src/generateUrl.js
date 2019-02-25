445b13b28ba854cefe6abc0f986d6cafca8f9f31
lourod1987 <louart87@gmail.com>
2019-02-24 22:28:58 -0600

    a commented out file of my attempt generate custom url's at click

---
+++ src/generateUrl.js
@@ -0,0 +1,15 @@
+  // generateUrl = (publisher, author, timestamp) => {
+  //   const tempArr = author.split(' ');
+  //   const newStr = tempArr.join('');
+  //   // const combine = `/@${publisher}/${newStr}/${timestamp}`;
+  //   const combine = "/@" + publisher + "/" + newStr + "/" + timestamp;
+  //   console.log("combine val: " + combine)
+  //   this.setState({
+  //     url: combine
+  //   })
+  //   console.log("curr state: " + this.state.url)
+  // }
+
+  // state = {
+  //   url: '/'
+  // }
