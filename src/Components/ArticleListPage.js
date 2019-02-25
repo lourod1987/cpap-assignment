2c468598e62345e599567f463c266b20deed6c7c
lourod1987 <louart87@gmail.com>
2019-02-24 22:48:11 -0600

    removing unused props

--- src/Components/ArticleListPage.js
+++ src/Components/ArticleListPage.js
@@ -6,11 +6,11 @@ import Footer from './Footer';
 export default class ArticleListPage extends Component {
     
   render() {   
-    const { articles, generateUrl, url, goToArticle } = this.props;
+    const { articles, goToArticle } = this.props;
     return (
         <div>
             <Navbar />
-            <Cards articles={articles} goToArticle={goToArticle} url={url} />
+            <Cards articles={articles} goToArticle={goToArticle} />
             {/* <Cards articles={articles} generateUrl={generateUrl} url={url} /> */}
             <Footer />
         </div>
