95dc26e766c81e457de00f7afd6a13508a5c01e4
lourod1987 <louart87@gmail.com>
2019-02-25 02:12:46 -0600

    removing uneeded code / cleanup.

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -6,23 +6,15 @@ export default class Cards extends Component {
     const { articles, goToArticle } = this.props;
     return (
       <div>
-        {/* <div>
-            {articles.map( articles => (
-                <img src={article} alt=""/>
-                <p>Company Name Filler</p>
-            ))}
-        </div> */}
         <ul className="cards">
           {articles.map( (article, index) => (
             <li key={`${article.publishedAt}+`}>
-              <Link to="/article">
+              <Link to="/article" className="article-lnk">
                 <img src={article.urlToImage} onClick={() => goToArticle(index)} alt=""/>
-                {/* <img src={article.urlToImage} onClick={() => this.props.generateUrl(article.source.name, article.author, article.publishedAt)} alt=""/> */}
+                <h2>{article.title}</h2>
+                <h3>{`${article.source.name} - ${article.author}`}</h3>
+                <p>{article.content}</p>
               </Link>
-              {/* <p>Hashtag Temp</p> */}
-              <h2>{article.title}</h2>
-              <h3>{`${article.source.name} - ${article.author}`}</h3>
-              <p>{article.content}</p>
             </li>
           ))}
         </ul>
