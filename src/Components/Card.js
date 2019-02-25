854ed4c0192bc88adfa24e393f8f13481e78f0f1
lourod1987 <louart87@gmail.com>
2019-02-25 04:26:24 -0600

    removing uneeded code. Adding in message to notify users to go to home page if they refresh on this screen

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 export default class Card extends Component {
   render() {
-    const { article } = this.props;
+    const { article, fullArticle } = this.props;
     return (
       <div>
-        <ul className="cards">
-          <li>
-            <a href={article.url} target="_blank" rel="noopener noreferrer">
-              <img src={article.urlToImage} alt=""/>
+        {(Object.entries(article).length === 0) ?
+        <h3 className="refresh-warn">Start from homepage to load an individual article</h3> : (
+          <ul className="card">
+            <li>
+              <a href={article.url} target="_blank" rel="noopener noreferrer" onClick={() => fullArticle(article.title)}>
+                <img src={article.urlToImage} alt=""/>
+              </a>
+              <h2>{article.title}</h2>
+              <h3>{article.source.name + " - " + article.author}</h3>
+              <p>{article.content}</p>
+            </li>
+            <a href={article.url} target="_blank" rel="noopener noreferrer" className="external-lnk" onClick={() => fullArticle(article.title)}>
+              <button className="external-btn">View on Engagdet</button>
             </a>
-            {/* <p>Hashtag Temp</p> */}
-            <h2>{article.title}</h2>
-            <h3>{article.name + " - " + article.author}</h3>
-            <p>{article.content}</p>
-          </li>
-        </ul>
-        <a href={article.url} target="_blank" rel="noopener noreferrer">
-          <button>View on Engagdet</button>
-        </a>
+          </ul>
+        )}
       </div>
 
     );
