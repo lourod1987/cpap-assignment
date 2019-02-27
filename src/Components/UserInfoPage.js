33a987ee581eff656ce0d80fa893a28e6a30a3e5
lourod1987 <louart87@gmail.com>
2019-02-27 03:55:17 -0600

    adding in variables, one for counting table numbers and one to create unique keys for map iteration. Removed uneeded prop. Removed ol setup and implemented a table.

--- src/Components/UserInfoPage.js
+++ src/Components/UserInfoPage.js
@@ -3,42 +3,38 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 
 export default class UserInfoPage extends Component {
+
   render() {
-    const { visited, viewedFull } = this.props;
+    let x = 0;
+    let i = -1;
+    const { visited } = this.props;
     return (
       <div>
         <Navbar />
         <main className="maininfo">
-          <h2>User Info</h2>
+         <h2>User Info</h2>
           {visited.length === 0 ?
-          (<div className="no-results">
-            <p>User has visited the following articles:</p>
-            <p>No results yet</p>
-          </div>)
-          :
-          (<ol>
-            <p>User has visited the following articles:</p>
-            {visited.map( (v) => (
-              <li key={v.viewed}>
-                <p>{`${v.viewed}`}</p>
-                
-              </li>
-            ))}
-          </ol>)}
-          {viewedFull.length === 0 ?
-          (<div className="no-results">
-            <p>User has viewed the following full articles at Engagdet:</p>
-            <p>No results yet</p>
-          </div>)
-          :
-          (<ol>
-              <p>User has viewed the following full articles at Engagdet:</p>
-            {viewedFull.map( (vF) => (
-              <li key={vF.fullArticle}>
-                <p>{`${vF.fullArticle}`}</p>
-              </li>
-            ))}
-          </ol>)}
+          (<p className="no-results">No results yet</p>)
+          :(<table>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Article Title</th>
+                  <th>Visited Article Page</th>
+                  <th>Seen Full Article on Engagdet</th>
+                </tr>
+              </thead>
+              <tbody>
+                {visited.map( (v) => (
+                <tr key={`${v.viewed}&${i+=1}`}>
+                  <td>{x+=1}</td>
+                  <td>{v.viewed}</td>
+                  <td>{v.seen}</td>
+                  <td>{v.fullArticle}</td>
+                </tr>
+                ))}
+              </tbody>
+          </table>)}
         </main>
         <Footer />
       </div>
