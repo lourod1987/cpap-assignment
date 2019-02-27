3585a1ca530293023dee7ca2d41e926022939c3c
lourod1987 <louart87@gmail.com>
2019-02-27 03:49:52 -0600

    adding new prop setFullArticle func.

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 
 export default class Card extends Component {
   render() {
-    const { article, fullArticle } = this.props;
+    const { article, setFullArticle } = this.props;
     return (
       <div>
         {(Object.entries(article).length === 0) ?
         <h3 className="refresh-warn">Start from homepage to load an individual article</h3> : (
           <ul className="card">
             <li>
-              <a href={article.url} target="_blank" rel="noopener noreferrer" onClick={() => fullArticle(article.title)}>
+              <a href={article.url} target="_blank" rel="noopener noreferrer" onClick={() => setFullArticle()}>
                 <img src={article.urlToImage} alt=""/>
               </a>
               <h2>{article.title}</h2>
               <h3>{article.source.name + " - " + article.author}</h3>
               <p>{article.content}</p>
             </li>
-            <a href={article.url} target="_blank" rel="noopener noreferrer" className="external-lnk" onClick={() => fullArticle(article.title)}>
+            <a href={article.url} target="_blank" rel="noopener noreferrer" className="external-lnk" onClick={() => setFullArticle()}>
               <button className="external-btn">View on Engagdet</button>
             </a>
           </ul>
