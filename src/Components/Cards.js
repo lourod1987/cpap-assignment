fc808cc0c2219f0b4701b0e5765d4b4e29693c39
lourod1987 <louart87@gmail.com>
2019-02-27 21:59:38 -0600

    adding in the main tag and classes to create a background with color

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -6,19 +6,21 @@ export default class Cards extends Component {
     let x = 0;
     const { articles, goToArticle } = this.props;
     return (
-      <div>
-        <ul className="cards">
-          {articles.map( (article, index) => (
-            <li key={`${article.publishedAt}+${x+=1}`}>
-              <Link to="/article" className="article-lnk">
-                <img src={article.urlToImage} onClick={() => goToArticle(index)} alt=""/>
-                <h2>{article.title}</h2>
-                <h3>{`${article.source.name} - ${article.author}`}</h3>
-                <p>{article.content}</p>
-              </Link>
-            </li>
-          ))}
-        </ul>
+      <div className="main-cards-bg">
+        <main className="main-cards-board">
+          <ul className="cards">
+            {articles.map( (article, index) => (
+              <li key={`${article.publishedAt}+${x+=1}`}>
+                <Link to="/article" className="article-lnk">
+                  <img src={article.urlToImage} onClick={() => goToArticle(index)} alt=""/>
+                  <h2>{article.title}</h2>
+                  <h3>{`${article.source.name} - ${article.author}`}</h3>
+                  <p>{article.content}</p>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </main>
       </div>
     );
   }
