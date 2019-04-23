b32c135f94423ccdbe503f170907faba75199c1e
lourod1987 <louart87@gmail.com>
2019-04-23 00:26:24 -0500

    Feat - setting up new headlines prop to be an optional output of call to render /article page

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 
 export default class Card extends Component {
   render() {
-    const { article, setFullArticle } = this.props;
+    const { headlines, article, setFullArticle } = this.props;
     return (
       <div>
         {(Object.entries(article).length === 0) ?
         <h3 className="refresh-warn">Start from homepage to load an individual article</h3> : (
           <ul className="card">
             <li>
-              <a href={article.url} target="_blank" rel="noopener noreferrer" onClick={() => setFullArticle()}>
-                <img src={article.urlToImage} alt=""/>
+              <a href={article.url || headlines.url} target="_blank" rel="noopener noreferrer" onClick={() => setFullArticle()}>
+                <img src={article.urlToImage || headlines.urlToImage} alt=""/>
               </a>
-              <h2>{article.title}</h2>
-              <h3>{article.source.name + " - " + article.author}</h3>
-              <p>{article.content}</p>
+              <h2>{article.title || headlines.title}</h2>
+              <h3>{article.source.name + " - " + article.author || headlines.source.name + " - " + headlines.author}</h3>
+              <p>{article.content || headlines.content}</p>
             </li>
-            <a href={article.url} target="_blank" rel="noopener noreferrer" className="external-lnk" onClick={() => setFullArticle()}>
-              <button className="external-btn">View on Engagdet</button>
+            <a href={article.url || headlines.url} target="_blank" rel="noopener noreferrer" className="external-lnk">
+              <button className="external-btn" onClick={() => setFullArticle()}>View on Engagdet</button>
             </a>
           </ul>
         )}
