a015276f72cebb78b5ec633b03b3bc43023fc452
lourod1987 <louart87@gmail.com>
2019-04-23 00:21:58 -0500

    Feat - importing loading gif to conditionally render while fetch's complete. Adding HeadlineStories component passing related props.

--- src/Components/ArticleListPage.js
+++ src/Components/ArticleListPage.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
+import HeadlineStories from './HeadlineStories';
 import Cards from './Cards';
 import Footer from './Footer';
+import loading from '../imgs/transparent-loading-4.gif';
 
 export default class ArticleListPage extends Component {
     
   render() {   
-    const { articles, goToArticle } = this.props;
+    const { headlines, articles, goToArticle } = this.props;
     return (
         <div>
             <Navbar />
-            <Cards articles={articles} goToArticle={goToArticle} />
+            {headlines.length === 0 ?
+            (<img src={loading} className="no-results" alt="" />) :
+            (<HeadlineStories headlines={headlines} goToArticle={goToArticle}/>)}
+            {articles.length === 0 ?
+              (<img src={loading} className="no-results" alt="" />) :
+              (<Cards articles={articles} goToArticle={goToArticle} />)}
             <Footer />
         </div>
     );
