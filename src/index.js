01c9bbf0bc9800b52c63db0e0818c3c3142586ac
lourod1987 <louart87@gmail.com>
2019-04-26 03:08:44 -0500

    Feat - importing necessary files for creating a redux store and saving the stores state. Testing with manual dispatch actions.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,30 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/ui/App';
 import * as serviceWorker from './serviceWorker';
+import C from './constants';
+import storeFactory from './store';
+import { setArticle } from './actions';
+
+const initialState = (localStorage['redux-store']) ? JSON.parse(localStorage['redux-store']) :
+  {};
+
+const saveState = () => {
+  const state = JSON.stringify(store.getState());
+  localStorage['redux-store'] = state;
+}
+
+const store = storeFactory(initialState);
+
+store.subscribe(saveState);
+
+store.dispatch({
+  type: C.ADD_ERROR,
+  payload: 'this is a test error'
+})
+
+store.dispatch(
+  setArticle("This is a crazy tech story title","Engadget", "Lou Ye", "snazzy clickbait here", "some long paragraph here", "img link here", "full article link here")
+)
 
 ReactDOM.render(
   <BrowserRouter><App /></BrowserRouter>,
