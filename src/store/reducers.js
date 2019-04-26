5ebeabd1a90d548111f105deb461c1cc4de030e9
lourod1987 <louart87@gmail.com>
2019-04-25 23:09:20 -0500

    Feat - adding in errors reducer to handle both adding and clearing errors

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -5,3 +5,13 @@ export const article = (state={}, action) =>
     action.payload :
     state;
 
+export const errors = (state=[], action) => {
+  switch(action.type) {
+    case C.ADD_ERROR:
+      return [...state, action.payload];
+    case C.CLEAR_ERROR:
+      return state.filter( (err, index) => index !== action.payload);
+    default:
+      return state;
+  }
+}
