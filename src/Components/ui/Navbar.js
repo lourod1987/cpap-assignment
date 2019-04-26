0b08a7603a89ba80a370eea9b8828267e070afb0
lourod1987 <louart87@gmail.com>
2019-04-25 22:16:55 -0500

    Feat - Importing and adding in logo to the navbar. Adding in link to search page

--- src/Components/ui/Navbar.js
+++ src/Components/ui/Navbar.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../imgs/TechAgg_Logo_v3.png'
 
 export default class Navbar extends Component {
   render() {
     return (
       <header>
         <nav>
-          <img src="" alt=""/>
-          <Link to="/" className="site-name"><h1>TechAgg</h1></Link>
-          {/* <input type="text"  placeholder="Search"/> */}
+          <img src={logo} alt="site logo - green lightning symbol" className="nav-logo"/>
+          <Link to="/" className="site-name">
+            <h1>TechAgg</h1>
+          </Link>
+          <Link to="/search" className="search-link">Search</Link>
           <Link to="/userinfo" className="link">User Info</Link>
         </nav>
       </header>
