bcdc14a043c3610207c3803af9c23b5264e7ae98
lourod1987 <louart87@gmail.com>
2019-04-25 21:55:10 -0500

    Feat - adding in new fetch to constants and creating a list of needed app actions

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,16 @@
 const constants = {
+  FETCH_TOPSTORIES: "FETCH_TOPSTORIES",
   FETCH_ARTICLES: "FETCH_ARTICLES",
   SET_ARTICLE_TO_DISPLAY: "SET_ARTICLE_TO_DISPLAY",
   SET_VISITED_ARTICLE: "SET_VISITED_ARTICLE",
   SET_SEEN_ENGAGDET_ARTICLE: "SET_SEEN_ENGAGDET_ARTICLE"
 }
+
+// fetch top stories
+// fetch articles
+// select article to display
+// set visited articles
+// set seen articles
+// 
 
 export default constants;
