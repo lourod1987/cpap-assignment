b0e654366e09f612d1acd49abb9d46c9e6660b27
lourod1987 <louart87@gmail.com>
2019-04-26 03:05:04 -0500

    Feat - creating middleware name console messages which shows the dispatched action and additional information on each dispatch

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import C from '../constants';
+import appReducer from './reducers';
+import { createStore, applyMiddleware } from 'redux';
+
+const consoleMesssages = store => next => action => {
+  let result;
+
+  console.groupCollapsed(`dispatching action => ${action.type}`);
+  console.log('article: ', store.getState().setArticle);
+
+  result = next(action);
+
+  let { setArticle, errors } = store.getState();
+
+  console.log(`
+    article: ${JSON.stringify(setArticle)}
+    errors: ${errors.length}
+  `);
+  console.groupEnd();
+
+  return result;
+}
+
+export default (initialState={}) => {
+  return applyMiddleware(consoleMesssages)(createStore)(appReducer, initialState);
+}
