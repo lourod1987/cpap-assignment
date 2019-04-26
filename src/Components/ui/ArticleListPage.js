0a0dcfb3469405c81432c9e5da05e89d7642e63e
lourod1987 <louart87@gmail.com>
2019-04-25 22:30:10 -0500

    Feat - adding in state of type to pass as a prop to Cards

--- src/Components/ui/ArticleListPage.js
+++ src/Components/ui/ArticleListPage.js
@@ -6,18 +6,22 @@ import Footer from './Footer';
 import loading from '../../imgs/transparent-loading-4.gif';
 
 export default class ArticleListPage extends Component {
+  state = {
+    type: "main"
+  }
     
   render() {   
+    const { type } = this.state;
     const { headlines, articles, goToArticle } = this.props;
     return (
         <div>
             <Navbar />
             {headlines.length === 0 ?
-            (<img src={loading} className="no-results" alt="" />) :
+            null :
             (<HeadlineStories headlines={headlines} goToArticle={goToArticle}/>)}
             {articles.length === 0 ?
               (<img src={loading} className="no-results" alt="" />) :
-              (<Cards articles={articles} goToArticle={goToArticle} />)}
+              (<Cards cards={articles} goToArticle={goToArticle} type={type}/>)}
             <Footer />
         </div>
     );
