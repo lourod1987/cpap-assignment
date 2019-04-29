4a218e010129130cb924192709c5ea2ddd14b13e
lourod1987 <louart87@gmail.com>
2019-04-29 14:47:47 -0500

    Feat - added in generation of dynamic links

--- src/components/ui/Cards.js
+++ src/components/ui/Cards.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Cards = props => {
-  let x = 0;
   const { cards, goToArticle, type } = props;
+
+  let articleId = cards.map( a => a.title.split(' '));
+  // let articleLink = articleId.toLowerCase();
+  // console.log(articleLink[1].join('-'))
   return (
     <div className="main-cards-bg">
       <main className="main-cards-board">
         <ul className="cards">
-          {cards.map( (article, index) => (
-            <li key={`${article.publishedAt}+${x+=1}`} onClick={() => goToArticle(index, type, article.title)}>
-              <Link to="/article" className="article-lnk">
+          {cards.map( (article, index) => 
+            (
+            <li key={`${article.publishedAt}+${index}`} onClick={() => goToArticle(index, type, article.title)}>
+              <Link to={`/article/${articleId[index].join('-').toLowerCase()}`} className="article-lnk">
                 <img src={article.urlToImage} alt=""/>
                 <h2>{article.title}</h2>
                 <h3>{`${article.source.name} - ${article.author}`}</h3>
