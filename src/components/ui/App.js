4c929ab6416edf1330966d1f4adda6ee9b4534fe
lourod1987 <louart87@gmail.com>
2019-04-29 14:44:45 -0500

    Refactor - remove commented out import

--- src/components/ui/App.js
+++ src/components/ui/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ArticleContainer from '../containers/ArticleContainer';
 import '../../stylesheets/App.scss';
-// import logo from '../../logo.svg';
 
 const App = () => {
   return (
-    <ArticleContainer />
+    <ArticleContainer /> 
   );
 }
 
