0372a34153a89147897c6fc45475d4b3f6653695
lourod1987 <louart87@gmail.com>
2019-05-01 18:32:20 -0500

    Chore - remove comment list of required constants

--- src/constants.js
+++ src/constants.js
@@ -11,11 +11,4 @@ const constants = {
   CLEAR_ERROR: "CLEAR_ERROR"
 }
 
-// fetch top stories
-// fetch articles
-// select article to display
-// set visited articles
-// set seen articles
-// 
-
 export default constants;
